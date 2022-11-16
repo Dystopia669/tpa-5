d7c2bb8489efd75e50f6654b6e45dd85fbc4fbc9
WonderfulLife2121 <nuralif34466@gmail.com>
2022-11-16 17:34:54 +0700

    upload tpa 5 terbaru

--- userRoute/todo.js
+++ userRoute/todo.js
@@ -38,22 +38,51 @@ router.get("/getTodo", (req, res) => {
 router.get("/getTodo/:id", async (req, res) => {
     try {
         const getTodoById = await todoSchema.findById(req.params.id)
-        .then(doc => {
-            if(doc){
-                return res.status(200).json({doc})
-                res.send(getTodoById)
+        .then(getTodoById => {
+            if(getTodoById){
+                return res.status(200).json(getTodoById)
             }else {
                 return res.status(404)
             }
         })
-        
     } catch (error) {
         res.send(error)
     }
 })
 // mengubah todo
-router.put("/updateTodo", async (req, res) =>{
-    res.send("cek update todo")
+router.put("/updateTodo/:id",validationToken, async (req, res) => {
+    var updateTodo = {_id: req.params.id}
+    try {
+        todo.updateOne(updateTodo, req.body)
+    .then(doc => {
+        if(doc){
+            return res.status(200).json(doc)
+        }
+        else {
+            res.status(404)
+        }
+    })  
+    } catch (error) {
+        res.send(error)
+    }
+    
+})
+// menghapus todo
+router.delete("/deleteTodo/:id", async (req,res) => {
+    try {
+        todo.findByIdAndDelete(req.params.id)
+        .then(doc => {
+            if(doc){
+                return res.status(200).json(doc)
+            }
+            else{
+                res.status(404)
+            }
+        })
+        
+    } catch (error) {
+        res.send("id todo not found")
+    }
 })
 
 module.exports = router
